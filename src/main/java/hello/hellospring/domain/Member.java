a806de9278bd6532b554c60da0d86f76013e066e
TaeYing <https://github.com/TaeYing>
2021-04-22 16:41:58 +0900

    스프링부트 연습용

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //@Column(name = "username")
     private String name;
     public Long getId() {
         return id;
@@ -16,4 +22,5 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
+
 }
